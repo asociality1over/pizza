1d9d96f464ed8530697422adddd9ffe31b352e9c
bruhlord-s <treize.konstantin@yandex.ru>
2021-04-28 21:12:13 +0600

    Hачал делать корзину через Redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './tailwind.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(  
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
