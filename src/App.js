ef336cacd576a2d60cae1e4f66badcd158050e98
bruhlord-s <treize.konstantin@yandex.ru>
2021-05-03 20:38:34 +0600

    Добавлена страница Корзины

--- src/App.js
+++ src/App.js
@@ -44,7 +44,9 @@ function Header(props) {
         </nav>
         
         <div className="buttons flex justify-around w-1/6">
-          <Button text="Корзина" />
+          <Link to={'/cart'}>
+            <Button text="Корзина" />
+          </Link>
           
           <Link to={'/login'}>
             <Button text="Войти" arrow="true"/>
@@ -229,6 +231,34 @@ function Login() {
   );
 }
 
+function Cart () {
+  return(
+    <div className="dark:bg-gray-800 flex flex-col justify-between h-screen">
+      <div>
+        <Header />
+        
+        <div class="container mx-auto mt-10">
+        
+            <div class="w-full dark:bg-gray-800 px-10 py-10">
+              <div class="flex justify-between border-b pb-8">
+                <h1 class="font-semibold text-2xl dark:text-gray-100">Корзина</h1>
+                <h2 class="font-semibold text-2xl dark:text-gray-100">0 товаров</h2>
+              </div>
+              <div class="flex mt-10 mb-5">
+                <h3 class="font-semibold dark:text-gray-100 text-gray-800 text-xs uppercase w-2/5">О товаре</h3>
+                <h3 class="font-semibold dark:text-gray-100 text-gray-800 text-xs uppercase w-1/5 text-center">Количество</h3>
+                <h3 class="font-semibold dark:text-gray-100 text-gray-800 text-xs uppercase w-1/5 text-center">Цена</h3>
+                <h3 class="font-semibold dark:text-gray-100 text-gray-800 text-xs uppercase w-1/5 text-center">Итог</h3>
+              </div>
+            </div>
+          
+        </div>
+      </div>
+      <Footer />
+      </div>
+  );
+}
+
 
 function App() {
   return(    
@@ -236,6 +266,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/cart" component={Cart} />
       </Switch>
     </Router>
   );
